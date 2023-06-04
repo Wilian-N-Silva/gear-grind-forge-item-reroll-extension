2e8ed9b5fe9d23599ce0a865571e5902d93d0115
Wilian-N-Silva <wilian.silva209@gmail.com>
2023-06-04 15:24:25 -0300

    feat: :sparkles: Button builder feature

--- scripts/main.js
+++ scripts/main.js
@@ -24,9 +24,60 @@ const GG_ITEM_MODIFIERS = {
   },
 }
 
+
+
 var isLoaded = false
 
-const buildItemButtons = (ITEM) => { }
+const rerollItem = () => { }
+
+const buildRerollButton = (DATA) => {
+  const BUTTON = document.createElement('button')
+  const BUTTON_STYLE = 'border: none; background: transparent; color: white; margin-left: 8px; margin-right: 16px; cursor: pointer'
+
+  BUTTON.setAttribute('type', 'button')
+  BUTTON.setAttribute('data-item-id', DATA.itemID)
+  BUTTON.addEventListener('click', rerollItem)
+  BUTTON.setAttribute('data-next-reroll', DATA.nextReroll)
+  BUTTON.innerText = DATA.label
+  BUTTON.style = BUTTON_STYLE
+
+  return BUTTON
+}
+
+const handleButtonData = (LABEL, ITEM_ID, NEXT_REROLL) => {
+  const BTN_DATA = {
+    'label': LABEL,
+    'itemID': ITEM_ID,
+    'nextReroll': NEXT_REROLL,
+  }
+
+  return BTN_DATA
+}
+
+const buildItemButtons = (ITEM) => {
+  const ITEM_NAME_EL = ITEM.querySelector('span')
+  const ITEM_ID = ITEM_NAME_EL.getAttribute('item_id')
+  const ITEM_CLASSLIST = ITEM_NAME_EL.classList
+
+  if (!ITEM_CLASSLIST.contains(GG_RARITY_CLASSES.legendary)) {
+    const NEXT_REROLL = !ITEM_CLASSLIST.contains(GG_RARITY_CLASSES.common) && !ITEM_CLASSLIST.contains(GG_RARITY_CLASSES.legendary) ? 'CUBE' : 'CYLINDER'
+
+    const BTN_REROLL_RARITY_DATA = handleButtonData('Reroll Rarity', ITEM_ID, NEXT_REROLL)
+
+    const BTN_REROLL_RARITY = buildRerollButton(BTN_REROLL_RARITY_DATA)
+
+    ITEM.appendChild(BTN_REROLL_RARITY)
+  }
+
+  const BTN_REROLL_STATS_DATA = handleButtonData('Reroll Stats', ITEM_ID, 'PYRAMID')
+  const BTN_REROLL_STATS_VALUES_DATA = handleButtonData('Reroll Values', ITEM_ID, 'PRISM')
+
+  const BTN_REROLL_STATS = buildRerollButton(BTN_REROLL_STATS_DATA)
+  const BTN_REROLL_STATS_VALUES = buildRerollButton(BTN_REROLL_STATS_VALUES_DATA)
+
+  ITEM.appendChild(BTN_REROLL_STATS)
+  ITEM.appendChild(BTN_REROLL_STATS_VALUES)
+}
 
 const initRerollOptions = () => {
   const FORGE_FORM = document.querySelector('.forge_form')
